54fc68081f2ec1351d56a6fa637074c223d0be2e
vkokare45 <notaloc778@duck.com>
2026-01-06 08:06:59 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,9 +1,10 @@
-api_id = 22779671
-api_hash = "125d8d88b77309dc3b154cbbfc2dacb2"
+api_id = 22694914
+api_hash = "4fd02c02368941a03d48ecab3a3b4a31"
 bot_token = "6948815452:AAHLdIf8gKLDp_MMkt8iSJ6wLOTdsAsIq-I"
-auth_users = [1713924419]
-sudo_user = 1713924419
+auth_users = [7491374623]
+sudo_user = 7491374623
 log_channel = -1002143629860
 txt_channel = -1002049391187
+
 
 
